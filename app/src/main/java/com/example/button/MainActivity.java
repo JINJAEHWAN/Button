bc9141af17998604aee32afd703c0bc4a89a030a
JINJAEHWAN <jinjh99@gmail.com>
2021-07-12 21:47:54 +0900

    Button event

--- app/src/main/java/com/example/button/MainActivity.java
+++ app/src/main/java/com/example/button/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     //뷰의 주소 값을 담을 참조 변수
     TextView text1;
-    Button button1, button2;
+    Button button1, button2, button3,button4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,12 +20,17 @@ public class MainActivity extends AppCompatActivity {
         text1 =(TextView)findViewById(R.id.textView);
         button1 = (Button)findViewById(R.id.button);
         button2 = (Button)findViewById(R.id.button2);
+        button3 = (Button)findViewById(R.id.button3);
+        button4 = (Button)findViewById(R.id.button4);
         //리스너 객체를 생성
         BtnListener1 listener1 = new BtnListener1();
         BtnListener2 listener2 = new BtnListener2();
+        BtnListener34 listener34 = new BtnListener34();
         //리스너를 버튼 객체에 설정한다.
         button1.setOnClickListener(listener1);
         button2.setOnClickListener(listener2);
+        button3.setOnClickListener(listener34);
+        button4.setOnClickListener(listener34);
     }
     //첫 번째 버튼과 연결된 리스너
     class BtnListener1 implements View.OnClickListener{
@@ -41,4 +46,20 @@ public class MainActivity extends AppCompatActivity {
             text1.setText("두 번째 버튼을 눌렀습니다.");
         }
     }
+    class BtnListener34 implements View.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            //이벤트가 발생된 뷰의 ID 값을 추출한다.
+            int id = v.getId();
+            //id로 분기한다.
+             switch(id){
+                 case R.id.button3:
+                     text1.setText("세 번째 버튼을 눌렀습니다.");
+                     break;
+                 case R.id.button4:
+                     text1.setText("네 번째 버튼을 눌렀습니다.");
+             }
+        }
+    }
+
 }
